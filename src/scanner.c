2c5fd53b1ff7de3f3c8beddcee5774f338cf89c6
AvhiMaz <avhimazumder5@outlook.com>
2026-05-16 18:23:21 +0530

    feat: detect undercollateralized positions and push to `liq_queue`
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/scanner.c
+++ src/scanner.c
@@ -2,6 +2,7 @@
 #include "market.h"
 #include "position.h"
 #include <stdint.h>
+#include <stdlib.h>
 
 void scanner(RiskEngine *engine) {
     for (uint32_t i = 0; i < engine->position_count; i++) {
@@ -13,17 +14,33 @@ void scanner(RiskEngine *engine) {
         int64_t unrealized_pnl;
 
         if (pos->side == LONG) {
-            // mark_price - entry_price * size
             unrealized_pnl =
                 ((int64_t)market->mark_price - (int64_t)pos->entry_price) *
                 (int64_t)pos->size;
         } else {
-            // entry_price - mark_price * size
             unrealized_pnl =
                 ((int64_t)pos->entry_price - (int64_t)market->mark_price) *
                 (int64_t)pos->size;
         }
 
         int64_t equity = (int64_t)pos->margin + unrealized_pnl;
+
+        int64_t position_value =
+            (int64_t)market->mark_price * (int64_t)pos->size;
+        int64_t health = (equity * 100) / position_value;
+
+        if (health < market->config.maintenance_margin_rate) {
+            pos->state = LIQUIDATING;
+
+            if (engine->liq_queue_count == engine->liq_queue_cap) {
+                engine->liq_queue_cap *= 2;
+                engine->liq_queue =
+                    realloc(engine->liq_queue,
+                            engine->liq_queue_cap * sizeof(Position *));
+            }
+
+            engine->liq_queue[engine->liq_queue_count] = pos;
+            engine->liq_queue_count++;
+        }
     }
 }
