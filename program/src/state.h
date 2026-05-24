8fdfe2bd86a0d3e684365210f480b2d9e11fb570
AvhiMaz <avhimazumder5@outlook.com>
2026-05-24 16:59:19 +0530

    feat(program): impl state
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

---
+++ program/src/state.h
@@ -0,0 +1,18 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <caravel.h>
+
+typedef struct {
+    Pubkey   trader;
+    uint64_t entry_price;
+    uint64_t size;
+    uint64_t margin;
+    uint64_t liquidation_price;
+    uint8_t  side;  // 0 = LONG, 1 = SHORT
+    uint8_t  state; // 0 = OPEN, 1 = LIQUIDATING, 2 = CLOSED
+    uint32_t market_index;
+    uint64_t opened_at;
+} PositionState;
+
+#endif
