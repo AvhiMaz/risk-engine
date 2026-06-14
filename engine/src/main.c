dc0048c7169811471cccb7165d7d8c1b185e4145
AvhiMaz <avhimazumder5@outlook.com>
2026-06-14 11:06:09 +0530

    feat: wire liquidator to submit real on-chain transactions
    engine now loads keypair and fetches open positions from devnet on
    startup via getProgramAccounts. liquidator thread builds and signs
    a liquidate_position transaction and submits it via sendTransaction
    instead of printing to stdout.
    
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- engine/src/main.c
+++ engine/src/main.c
@@ -1,39 +1,45 @@
 #include "engine.h"
 #include "feed.h"
+#include "keypair.h"
 #include "liquidator.h"
 #include "market.h"
-#include "position.h"
+#include "rpc.h"
 #include "scanner.h"
 #include <stdio.h>
+
+#include "../../thirdparty/libbase58/libbase58.h"
+
+#define PROGRAM_ID "44iCEak3d798Gau411YoDisRFNMyZuHM941oZWYfx95U"
 
 int main() {
     RiskEngine engine;
     engine_init(&engine);
 
+    char kp_path[256];
+    snprintf(kp_path, sizeof(kp_path), "%s/.config/solana/id.json",
+             getenv("HOME"));
+    if (load_keypair(&engine.liquidator_kp, kp_path) < 0) {
+        fprintf(stderr, "failed to load keypair from %s\n", kp_path);
+        return 1;
+    }
+
+    size_t sz = 32;
+    b58tobin(engine.program_id, &sz, PROGRAM_ID, 0);
+
+    Market sol_perp = {.market_index = 0,
+                       .mark_price = 0,
+                       .config = {.market_index = 0,
+                                  .initial_margin_rate = 10,
+                                  .maintenance_margin_rate = 5,
+                                  .max_leverage = 20}};
+    engine_add_market(&engine, &sol_perp);
+
+    if (rpc_get_program_accounts(&engine, PROGRAM_ID) < 0)
+        fprintf(stderr, "failed to fetch positions\n");
+
     pthread_t scanner_thread;
     pthread_t liquidator_thread;
     pthread_t feed_thread;
-
-    Market    sol_perp = {.market_index = 0,
-                          .mark_price = 100,
-                          .config = {.market_index = 0,
-                                     .initial_margin_rate = 10,
-                                     .maintenance_margin_rate = 5,
-                                     .max_leverage = 20}};
-
-    engine_add_market(&engine, &sol_perp);
-
-    Position pos = {.entry_price = 200,
-                    .size = 10,
-                    .side = LONG,
-                    .margin = 100,
-                    .market_index = 0,
-                    .state = OPEN,
-                    .liquidation_price = 180,
-                    .opened_at = 0,
-                    .trader_id = {0}};
-
-    engine_add_position(&engine, &pos);
 
     pthread_create(&scanner_thread, NULL, scanner, &engine);
     pthread_create(&liquidator_thread, NULL, liquidator, &engine);
